ed7c314b9ca94275925b1d4ca36b6bf0cbba7814
majhar14 <86432462+majhar14@users.noreply.github.com>
2021-06-27 10:04:41 -0500

    Update TestLogin.py

--- TestLogin.py
+++ TestLogin.py
@@ -1,25 +1,27 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
 import time
 
-# Home Page
+# Launch Home Page
+
 path = "C:\\Program Files (x86)\\chromedriver.exe"
 driver = webdriver.Chrome(path)
 driver.get("https://www.hudl.com/login")
 
-# Login credentials 
+# Login credentials
 
-login_page_email = driver.find_element_by_id("email")
-login_page_email.send_keys("majhar14@gmail.com")
-login_page_passwoord = driver.find_element_by_id("password")
-login_page_passwoord.send_keys("xxxxxxxx")
-login_button = driver.find_element_by_id("logIn")
-login_button.click()
-time.sleep(10)
-driver.close()
+try :
+
+  login_page_email = driver.find_element_by_id("email")
+  login_page_email.send_keys("majhar14@gmail.com")
+  login_page_passwoord = driver.find_element_by_id("password")
+  login_page_passwoord.send_keys("xxxxxxxx")
+  login_button = driver.find_element_by_id("logIn")
+  login_button.click()
+  time.sleep(10)
+  driver.close()
+
+except Exception : 
+  print ("Login Fail")
 
 
